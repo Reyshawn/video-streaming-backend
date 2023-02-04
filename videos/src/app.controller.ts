146ae411bd4814a041534d30235d4531efa6e6b6
Reyshawn <reshawnchang@gmail.com>
2023-02-04 15:37:45 +0800

    Add video streaming services;

--- videos/src/app.controller.ts
+++ videos/src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param, Res, Headers } from '@nestjs/common';
+import { Controller, Get, Param, Res, Headers, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 import type { Response } from 'express';
+import { statSync, createReadStream } from 'fs';
 
 @Controller()
 export class AppController {
@@ -11,10 +12,29 @@ export class AppController {
     return this.appService.getHello();
   }
 
-
   @Get('stream/:id')
-  async getStreamVideo(@Param('id') id: string, @Res({ passthrough: true }) res: Response) {
+  async getStreamVideo(@Param('id') id: string, @Headers() headers, @Res() res: Response) {
+    const videoPath = `assets/${id}.mp4`;
+    const { size } = statSync(videoPath);
+    const videoRange = headers.range;
 
-    return {id: id}
+    const CHUNK_SIZE = 10 ** 6 // 1MB
+
+    const parts = videoRange.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = Math.min(start + CHUNK_SIZE, size - 1)
+
+    const contentLength = end - start + 1
+
+    const readStreamfile = createReadStream(videoPath, { start, end, highWaterMark:60 });
+    const h = {
+      'Content-Range': `bytes ${start}-${end}/${size}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': contentLength,
+      'Content-Type': 'video/mp4'
+    }
+
+    res.writeHead(HttpStatus.PARTIAL_CONTENT, h) //206
+    readStreamfile.pipe(res)
   }
 }
