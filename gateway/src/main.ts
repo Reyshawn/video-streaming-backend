02b2834e8367d3c89f5b7bf0cd5e2b75af856f13
Reyshawn <reshawnchang@gmail.com>
2023-02-06 11:18:05 +0800

    Proxy the video service;

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,9 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {createProxyMiddleware} from 'http-proxy-middleware';
+import { JwtService } from '@nestjs/jwt';
+import { AuthModule } from './auth/auth.module';
+import { ExtractJwt } from 'passport-jwt';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
+
+  const service: JwtService = app.select(AuthModule).get(JwtService)
+
+  app.use(
+    '/api/video', 
+    createProxyMiddleware({
+      target: "http://localhost:3001",
+      changeOrigin: true,
+      pathRewrite: {'^/api/video' : '/'},
+      onProxyReq: (proxyReq, req, res, options) => {
+        try {
+          const token = ExtractJwt.fromAuthHeaderAsBearerToken()(req)
+          service.verify(token)
+        } catch (err) {
+          res.status(403).json({ message: 'Unauthorized!' })
+          return
+        }
+      }
+    })
+  )
+
   await app.listen(3000)
 }
 bootstrap();
